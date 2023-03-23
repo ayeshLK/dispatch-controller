0537becba36abc0b5e9b06a125256f66882339de
ayeshLK <ayesh9303@gmail.com>
2023-03-24 04:16:50 +0530

    Add initial project structure

---
+++ dispatch-controller-service/src/main/java/io/ayesh/sample/DispatchControllerServiceApp.java
@@ -0,0 +1,12 @@
+package io.ayesh.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchControllerServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatchControllerServiceApp.class, args);
+    }
+}
