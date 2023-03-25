668ad5d0137dede5d53b4ff495af67ac0f6d87da
ayeshLK <ayesh9303@gmail.com>
2023-03-26 02:27:56 +0530

    Improve the service API by adding supported content-type validation

--- dispatch-controller-service/src/main/java/io/ayesh/sample/Controller/DispatchController.java
+++ dispatch-controller-service/src/main/java/io/ayesh/sample/Controller/DispatchController.java
@@ -5,6 +5,7 @@ import io.ayesh.sample.model.Drone;
 import io.ayesh.sample.model.Medication;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,19 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.List;
 
-@RestController("/drones")
+@RestController
 public class DispatchController {
-    @PostMapping
+    @PostMapping(
+            value = "/drones",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     ResponseEntity<Drone> registerDrone(@Valid @RequestBody Drone drone) {
         return ResponseEntity.status(HttpStatus.CREATED).body(drone);
     }
 
-    @GetMapping
+    @GetMapping(
+            value = "/drones",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     ResponseEntity<List<Drone>> getDronesAvailableForLoading() {
         return ResponseEntity.ok(Collections.emptyList());
     }
 
-    @GetMapping("/{serialNumber}/battery-capacity")
+    @GetMapping(
+            value = "/drones/{serialNumber}/battery-capacity",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     ResponseEntity<BatteryCapacity> getBatteryCapacity(@PathVariable("serialNumber") String serialNumber) {
         BatteryCapacity batteryCapacity = new BatteryCapacity();
         batteryCapacity.setDroneSerialNumber("abc1");
@@ -35,12 +46,19 @@ public class DispatchController {
         return ResponseEntity.ok().body(batteryCapacity);
     }
 
-    @GetMapping("/{serialNumber}/medications")
+    @GetMapping(
+            value = "/drones/{serialNumber}/medications",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     ResponseEntity<List<Medication>> getMedications(@PathVariable("serialNumber") String serialNumber) {
         return ResponseEntity.ok().body(Collections.emptyList());
     }
 
-    @PostMapping("/{serialNumber}/medications")
+    @PostMapping(
+            value = "/drones/{serialNumber}/medications",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     ResponseEntity<String> loadMedication(@PathVariable("serialNumber") String serialNumber,
                                           @Valid @RequestBody List<Medication> medications) {
         return ResponseEntity.accepted().build();
