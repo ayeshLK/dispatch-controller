3c34a78cb08227f1315ab8759300968d11e5cf60
ayeshLK <ayesh9303@gmail.com>
2023-03-26 20:22:01 +0530

    Update sql schema script

--- dispatch-controller-service/src/main/resources/schema.sql
+++ dispatch-controller-service/src/main/resources/schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE drone (
     id INTEGER NOT NULL AUTO_INCREMENT,
     serial_number VARCHAR(100) NOT NULL UNIQUE,
     model ENUM('Lightweight', 'Middleweight', 'Cruiserweight', 'Heavyweight') NOT NULL,
-    weight_limit DECIMAL(5, 2) NOT NULL,
-    battery_capacity DECIMAL(3, 2) NOT NULL,
+    weight_limit DOUBLE NOT NULL,
+    battery_capacity DOUBLE NOT NULL,
     state ENUM('IDLE', 'LOADING', 'LOADED', 'DELIVERING', 'DELIVERED', 'RETURNING') NOT NULL,
     PRIMARY KEY (id)
 );
@@ -21,7 +21,7 @@ CREATE TABLE medication (
     shipment_id INTEGER NOT NULL,
     code VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    weight_limit DECIMAL(5, 2) NOT NULL,
+    weight_limit DOUBLE NOT NULL,
     image VARCHAR(100) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (shipment_id) REFERENCES shipment(id)
