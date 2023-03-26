d80ea68c7f50c87024c5577dbf134bd0e83edce6
ayeshLK <ayesh9303@gmail.com>
2023-03-27 00:39:05 +0530

    Include all the common-responses into a common-location

--- dispatch-controller-service/src/main/java/io/ayesh/sample/responses/ServiceResponses.java
+++ dispatch-controller-service/src/main/java/io/ayesh/sample/responses/ServiceResponses.java
@@ -1,8 +1,9 @@
-package io.ayesh.sample.exceptions;
+package io.ayesh.sample.responses;
 
 import java.util.List;
 
-public interface ErrorResponses {
+public interface ServiceResponses {
+    record CommonResponse(String message) {}
     record CommonErrorResponse(String message){}
     record ValidationErrorResponse(List<Error> errors) {}
     record Error(String fieldName, String message) {}
