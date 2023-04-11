fee1fb24c1750efbd2873ae9e0d24528fc9b7a2c
ayeshLK <ayesh9303@gmail.com>
2023-04-11 16:08:35 +0530

    Refactor the exception handler

--- dispatch-controller-service/src/main/java/io/ayesh/sample/exceptions/ApplicationExceptionHandler.java
+++ dispatch-controller-service/src/main/java/io/ayesh/sample/exceptions/ApplicationExceptionHandler.java
@@ -6,33 +6,29 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApplicationExceptionHandler {
 
     @ExceptionHandler(DataAccessException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
     ServiceResponses.CommonErrorResponse onDataAccessException(DataAccessException dataAccessException) {
         return new ServiceResponses.CommonErrorResponse(dataAccessException.getMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     ServiceResponses.CommonErrorResponse onHttpMessageNotReadable(HttpMessageNotReadableException messageNotReadable) {
         return new ServiceResponses.CommonErrorResponse(messageNotReadable.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     ServiceResponses.ValidationErrorResponse onMethodArgumentNotValid(
             MethodArgumentNotValidException argumentNotValid) {
         List<ServiceResponses.Error> errors = argumentNotValid
@@ -45,7 +41,6 @@ public class ApplicationExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     ServiceResponses.ValidationErrorResponse onConstraintViolation(ConstraintViolationException constraintViolation) {
         List<ServiceResponses.Error> errors = constraintViolation
                 .getConstraintViolations().stream()
@@ -57,14 +52,12 @@ public class ApplicationExceptionHandler {
 
     @ExceptionHandler(UnsupportedDroneStateException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     ServiceResponses.CommonErrorResponse onUnsupportedDroneState(UnsupportedDroneStateException unsupportedDroneState) {
         return new ServiceResponses.CommonErrorResponse(unsupportedDroneState.getMessage());
     }
 
     @ExceptionHandler(DroneOverloadedException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     ServiceResponses.CommonErrorResponse onDroneOverloaded(DroneOverloadedException droneOverloaded) {
         return new ServiceResponses.CommonErrorResponse(droneOverloaded.getMessage());
     }
